57afab68e338a3da4635fd906da4504b83495618
fatemeh razzaghy <fatemeh.razzaghy2005@gmail.com>
2025-10-17 16:28:59 +0330

    feat/about-us fix h in FeatureItemComponent

--- src/components/AboutUs/FeatureItemComponent.tsx
+++ src/components/AboutUs/FeatureItemComponent.tsx
@@ -1,4 +1,5 @@
-import type { Feature } from "@/types/aboutUs"
+// src/components/AboutUs/FeatureItemComponent.tsx
+import type { Feature } from "@/types/aboutUs";
 import { IconFromImageComponent } from "../Custom/IconFromImage/IconFromImageComponent";
 
 export function FeatureItem({ item }: { item: Feature }) {
@@ -6,11 +7,15 @@ export function FeatureItem({ item }: { item: Feature }) {
     <li className="py-4 sm:py-5">
       <div className="flex items-start gap-3 sm:gap-4">
         <div className="shrink-0 w-10 h-10 sm:w-12 sm:h-12 flex items-center justify-center">
-          <IconFromImageComponent src={item.icon}  />
+          <IconFromImageComponent src={item.icon} />
         </div>
         <div className="space-y-1">
-          <h4 className="font-semibold text-base sm:text-lg text-foreground">{item.title}</h4>
-          <p className="text-muted-foreground leading-6 text-sm sm:text-base">{item.text}</p>
+          <p className="font-semibold text-small sm:text-subtitle text-foreground">
+            {item.title}
+          </p>
+          <p className="text-muted-foreground leading-6 text-small sm:text-small">
+            {item.text}
+          </p>
         </div>
       </div>
     </li>
