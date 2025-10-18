79f25d8467c6d804984092d55d2976857eccb0e4
Sadra Sefidgari <47672827+Sdrf1445@users.noreply.github.com>
2025-10-18 14:45:18 +0330

    Fix/build errors (#13)
    * chore: remove unused variables for build
    
    * style: center link button section

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,15 +1,13 @@
 import AuthBackground from "@/components/Auth/Background/AuthBackground";
 
-import { useMobile } from "@/hooks/ResponsiveHooks";
 import LoginForm from "@/components/Auth/LoginForm/LoginForm";
 
 export default function Login() {
-  const isMobile = useMobile();
-  return (
-    <div className="w-screen h-screen bg-background overflow-hidden justify-center flex">
-      <AuthBackground />
+	return (
+		<div className="w-screen h-screen bg-background overflow-hidden justify-center flex">
+			<AuthBackground />
 
-      <LoginForm />
-    </div>
-  );
+			<LoginForm />
+		</div>
+	);
 }
