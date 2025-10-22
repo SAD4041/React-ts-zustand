696be857da892b5a4f9faaae4a4ae55f75d30fda
unknown <saman>
2025-10-23 00:59:27 +0330

    refactor:fuctionality searchbar

--- src/components/Profile/ProfileChallenges.tsx
+++ src/components/Profile/ProfileChallenges.tsx
@@ -17,10 +17,12 @@ import { Search } from "lucide-react";
 import CustomDropdown from "../Custom/CustomDropdown";
 const ProfileChallenges = () => {
   const challenges = [
-    { id: 1, name: "چالش یک",categoryID : 1 , category: "ورزشی" },
-    { id: 2, name: "چالش دو", categoryID : 3 , category: "علمی" },
-    { id: 3, name: "چالش سه", categoryID : 2 , category: "هنری" },
-    { id: 4, name: "چالش چهار", categoryID : 4 , category: "تفریحی" },
+    { id: 1, name: "چالش یک", categoryID: 1, category: "ورزشی" },
+    { id: 5, name: "چالش", categoryID: 1, category: "ورزشی" },
+    { id: 6, name: "یک", categoryID: 1, category: "ورزشی" },
+    { id: 2, name: "چالش دو", categoryID: 3, category: "علمی" },
+    { id: 3, name: "چالش سه", categoryID: 2, category: "هنری" },
+    { id: 4, name: "چالش چهار", categoryID: 4, category: "تفریحی" },
   ];
   const categories = [
     { id: 1, name: "ورزشی" },
@@ -28,31 +30,65 @@ const ProfileChallenges = () => {
     { id: 3, name: "علمی" },
     { id: 4, name: "تفریحی" },
   ];
-  const [checkedCategories, setCheckedCategories] = useState<{ [key: number]: boolean }>({});
+  const [checkedCategories, setCheckedCategories] = useState<{
+    [key: number]: boolean;
+  }>({});
+  const [search, setSearch] = useState("");
   const selectedCategoryIds = Object.keys(checkedCategories)
-  .filter((key) => checkedCategories[Number(key)])
-  .map(Number);
+    .filter((key) => checkedCategories[Number(key)])
+    .map(Number);
 
-const filteredChallenges =
-  selectedCategoryIds.length === 0
+  const searchedChallenges = search
     ? challenges
-    : challenges.filter((ch) => selectedCategoryIds.includes(ch.categoryID)); 
+        .filter((challenge) =>
+          challenge.name.toLowerCase().includes(search.toLowerCase())
+        )
+        .sort((a, b) => {
+          const aName = a.name.toLowerCase();
+          const bName = b.name.toLowerCase();
+          const s = search.toLowerCase();
+
+          // scoring
+          const score = (name: string) => {
+            if (name === s) return 3;
+            if (name.startsWith(s)) return 2;
+            return 1;
+          };
+
+          return score(bName) - score(aName);
+        })
+    : challenges;
+
+  const filteredChallenges =
+    selectedCategoryIds.length === 0
+      ? searchedChallenges
+      : searchedChallenges.filter((ch) =>
+          selectedCategoryIds.includes(ch.categoryID)
+        );
   return (
     <>
       <div className="flex justify-start items-center m-2.5 gap-2 w-full">
         <Formik
           initialValues={{ challengeSearch: "" }}
-          onSubmit={(values) => console.log(values)}
+          onSubmit={(values) => {
+            setSearch(values.challengeSearch);
+          }}
         >
-          {({ isSubmitting }) => (
-            <Form className="relative w-72 flex items-center">
+          {({ handleSubmit, isSubmitting }) => (
+            <Form
+              onSubmit={handleSubmit}
+              className="relative w-72 flex items-center"
+            >
               <CustomInput
-                icon={<Search className="text-[var(--color-blue-main)]" />}
+                icon={
+                    <button type="submit" className="flex items-center cursor-pointer">
+                      <Search className="text-[var(--color-blue-main)]" />
+                    </button>
+                }
                 name="challengeSearch"
                 label="جستجو"
-                className="pr-10" // add padding-right so text doesn't overlap button
+                className="pr-10"
               />
-
             </Form>
           )}
         </Formik>
