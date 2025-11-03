76451ed127a12db1dcd3ed6df9503319dbd0be55
amrayi <amirmohammadamrayi@gmail.com>
2025-11-03 17:48:03 +0330

    fix navigation and remove app.tsx

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,6 +2,9 @@ import { createBrowserRouter } from "react-router-dom";
 import PublicLayout from "@/layouts/PublicLayout/PublicLayout";
 import Landing from "@/pages/Landing";
 import Temp from "@/pages/Temp";
+import LoginForm from "@/pages/Login";
+import { Validation } from "@/pages/Validation";
+import { SignupForm } from "@/pages/SignUp";
 
 export const router = createBrowserRouter([
 	{
@@ -19,11 +22,23 @@ export const router = createBrowserRouter([
 				path: "/temp",
 				element: <Temp />,
 			},
-			// {
-			// 	path: "/AboutUs",
-			// 	element: <AboutUs />,
-			// },
-			// {
+			{
+				path: "/login",
+				element: <LoginForm />,
+			},
+			{
+				path: "/validation",
+				element: <Validation />,
+			},
+			{
+				path: "/signUp",
+				element: <SignupForm />,
+			},
+			{
+				path: "/validation",
+				element: <Validation />,
+			},
+			
 		],
 	},
 	// {
