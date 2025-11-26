f4721d0ea5f357daeeea6fb039a5d7f368927d08
soroush1384akhavan <soroush1384.akhavan@gmail.com>
2025-11-26 18:40:31 +0330

    feat: new backendurl

--- src/services/services.ts
+++ src/services/services.ts
@@ -13,7 +13,7 @@ import type {
 	PutParams,
 } from "../types/apiTypes";
 
-export const baseURL = "http://84.32.41.215:8080"; // backend URL
+export const baseURL = "https://api.elmocpc.ir"; // backend URL
 
 const apiClient: AxiosInstance = axios.create({
 	baseURL,
