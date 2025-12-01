bd2d3983ca0a9c64ccf855437cfb9575f0714861
it-hosein <hosseinmajidi7723@gmail.com>
2025-12-02 00:44:41 +0330

    Implementation of ContactUs page

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import PublicLayout from "@/layouts/PublicLayout/PublicLayout";
 import Landing from "@/pages/Landing";
 import Temp from "@/pages/Temp";
+import ContactUs from "@/pages/ContactUs";
 
 export const router = createBrowserRouter([
 	{
@@ -18,6 +19,10 @@ export const router = createBrowserRouter([
 			{
 				path: "/temp",
 				element: <Temp />,
+			},
+			{
+				path: "/ContactUs",
+				element: <ContactUs />,
 			},
 			// {
 			// 	path: "/AboutUs",
