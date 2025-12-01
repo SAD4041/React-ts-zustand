b17b473dc75b17a14d74eb85b299fbd982507019
MohammadAminBahari <84309493+MohammadAminBahari@users.noreply.github.com>
2025-12-01 23:15:34 +0330

    Feat/tabs component (#45)
    * feat: add tabs component from shadcn
    
    * feat: add underline to tabs text and remove its background
    
    * feat: add number circle to tab trgger
    
    * feat: make tab triggers scrollable horizontally

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -10,12 +10,12 @@ import { Form, Formik } from "formik";
 //import * as Yup from "yup";
 
 import {
-	Select,
-	SelectContent,
-	SelectGroup,
-	SelectItem,
-	SelectTrigger,
-	SelectValue,
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
 } from "@/components/Custom/Select/Select";
 import DatePicker from "@/components/Custom/DatePicker/DatePicker";
 import { PetDatePicker } from "@/components/Custom/PetDatePicker/PetDatePicker";
@@ -26,6 +26,7 @@ import PetKindToggleGroup from "@/components/PetRegister/PetKindToggleGroup";
 import Toggle from "@/components/Custom/Toggle/Toggle";
 import { useState } from "react";
 import * as Yup from "yup";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/Custom/Tabs/Tabs";
 import Address from "@/components/Custom/Address/Address";
 import { LocationSelector } from "@/components/Custom/Province/LocationSelector";
 import { Province } from "@/components/Custom/Province/Province";
@@ -35,15 +36,15 @@ import ServiceToggleGroup from "@/components/Booking/PetOwner/ServiceToggleGroup
 import ToggleGroupField from "@/components/Booking/PetOwner/ToggleGroupField";
 
 const validationSchema = Yup.object({
-	email: Yup.string()
-		.email("ایمیل معتبر نمی باشد")
-		.required("ایمیل اجباری است"),
-	password: Yup.string()
-		.min(6, "پسورد باید حداقل 6 کاراکتر باشد یسبشس سبسی بشسب")
-		.required("رمز عبور اجباری است"),
-	akhoond: Yup.string()
-		.min(6, "پسورد باید حداقل 6 کاراکتر باشد یسبشس سبسی بشسب")
-		.required("رمز عبور اجباری است"),
+  email: Yup.string()
+    .email("ایمیل معتبر نمی باشد")
+    .required("ایمیل اجباری است"),
+  password: Yup.string()
+    .min(6, "پسورد باید حداقل 6 کاراکتر باشد یسبشس سبسی بشسب")
+    .required("رمز عبور اجباری است"),
+  akhoond: Yup.string()
+    .min(6, "پسورد باید حداقل 6 کاراکتر باشد یسبشس سبسی بشسب")
+    .required("رمز عبور اجباری است"),
 });
 
 function Test() {
@@ -294,6 +295,20 @@ function Test() {
 			/>
 
 			{isChecked && <p>salllaaam</p>}
+      
+      <div className="flex w-200 flex-col gap-6">
+        <Tabs defaultValue="account">
+          <TabsList >
+            <TabsTrigger  value="account" number={3}>رزرو های فعال</TabsTrigger>
+            <TabsTrigger value="password" number={1}>رزرو های گذشته</TabsTrigger>
+			<TabsTrigger value="salam">رزرو های گذشته</TabsTrigger>
+          </TabsList>
+          <TabsContent  value="account">salam</TabsContent>
+          <TabsContent value="password">naaaaa</TabsContent>
+		  <TabsContent value="salam">naaaaa</TabsContent>
+        </Tabs>
+      </div>
+    </div>
 		</div>
 	);
 }
