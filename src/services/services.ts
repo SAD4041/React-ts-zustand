0329b6b7b21a50aaeba4cde82e56d47440ce0212
alijp84 <alijp84@gmail.com>
2025-12-14 16:11:51 +0330

    توضیح مختصر در مورد تغییرات انجام شده

--- src/services/services.ts
+++ src/services/services.ts
@@ -13,7 +13,7 @@ import type {
 	PutParams,
 } from "../types/apiTypes";
 
-export const baseURL = "http://1.2.3.4:8000"; // backend URL
+export const baseURL = import.meta.env.VITE_API_URL // backend URL
 
 const apiClient: AxiosInstance = axios.create({
 	baseURL,
