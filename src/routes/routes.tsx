603520f9de9ee8e2187bd310af2a5ae492f94b25
it-hosein <hosseinmajidi7723@gmail.com>
2025-12-29 04:53:30 +0330

    last efites

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,10 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
 import PublicLayout from "@/layouts/PublicLayout/PublicLayout";
-<<<<<<< HEAD
-import Landing from "@/pages/Landing";
-import Temp from "@/pages/Temp";
-import 	FAQ from "@/pages/FAQ";
-=======
 import LoginLayout from "@/layouts/PublicLayout/LoginLayout";
 import Home from "@/pages/Home";
 import Error404 from "@/pages/Error404";
@@ -12,123 +7,60 @@ import Error500 from "@/pages/Error500";
 import ProductListing from "@/components/ProductListing/productListing";
 import LoginForm from "@/pages/Login";
 import Validation from "@/pages/Validation";
-import SidebarLayout from "@/layouts/PublicLayout/SidebarLayout";
 import BrandProfileEditPage from "@/pages/BrandProfileEditPage";
-// import SidebarLayout from "@/layouts/PublicLayout/SidebarLayout";
 import WishlistPage from "@/pages/WishList";
->>>>>>> 0f6f3f9387680b400d5ad0a40c27c9eeb416f0ca
+import Temp from "@/routes/Temp";
+import FAQ from "@/pages/FAQ";
 
 export const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <PublicLayout />,
-        errorElement: (
-            <Error404 />
-        ),
-        children: [
-            {
-                index: true,
-                element: <Home />
-            },
-            {
-				path: "/brandProfileEdit",
-				element: <BrandProfileEditPage />,
-			},
-<<<<<<< HEAD
-			{
-				path: "/temp",
-				element: <Temp />,
-			},
-			{
-				path: "/FAQ",
-				element: <FAQ />,
-			},
-			
-			// {
-			// 	path: "/AboutUs",
-			// 	element: <AboutUs />,
-			// },
-			// {
-		],
-	},
-	// {
-	// 	element: <PrivateLayout />,
-	// 	children: [
-	// 		{
-	// 			path: "/EditProfile",
-	// 			element: <EditProfile />,
-	// 		},
-	// 		{
-	// 			path: "/DashBoard",
-	// 			element: <DashBoard />,
-	// 		},
-	// 	],
-	// },
-	// {
-	// 	element: <AnotherLayout />,
-	// 	children: [
-	// 		{
-	// 			path: "/login",
-	// 			element: <Login />,
-	// 		},
-	// 		{
-	// 			path: "/temp",
-	// 			element: <Temp />,
-	// 		},
-	// 	],
-	// },
-=======
-            {
-                path: "/error500",
-                element: <Error500 />
-            },
-            {
-                path: "/products-list",
-                element: <ProductListing />
-            },
-            {
-                path: "/dash/wishList",
-                element: <WishlistPage />,
-            },
-
-        ],
-    },
-
-    {
-        path: "/login",
-        element: <LoginLayout />,
-        errorElement: (
-            <Error404 />
-        ),
-        children: [
-            {
-                index: true,
-                element: <LoginForm />,
-            },
-            {
-                path: "/login/verify",
-                element: <Validation />,
-            },
-        ],
-    },
-    // {
-    //     path: "/dash",
-    //     element: <SidebarLayout />,
-    //     errorElement: <Error404 />,
-    //     children: [
-    //         // {
-    //         //     index: true,
-    //         //     element: <DashboardHome />,
-    //         // },
-    //         {
-    //             path: "/dash/wishList",
-    //             element: <WishlistPage />,
-    //         },
-    //         // {
-    //         //     path: "settings",
-    //         //     element: <Settings />,
-    //         // },
-    //     ],
-    // }
->>>>>>> 0f6f3f9387680b400d5ad0a40c27c9eeb416f0ca
+  {
+    path: "/",
+    element: <PublicLayout />,
+    errorElement: <Error404 />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/brandProfileEdit",
+        element: <BrandProfileEditPage />,
+      },
+      {
+        path: "/temp",
+        element: <Temp />,
+      },
+      {
+        path: "/FAQ",
+        element: <FAQ />,
+      },
+      {
+        path: "/error500",
+        element: <Error500 />,
+      },
+      {
+        path: "/products-list",
+        element: <ProductListing />,
+      },
+      {
+        path: "/dash/wishList",
+        element: <WishlistPage />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <LoginLayout />,
+    errorElement: <Error404 />,
+    children: [
+      {
+        index: true,
+        element: <LoginForm />,
+      },
+      {
+        path: "/login/verify",
+        element: <Validation />,
+      },
+    ],
+  },
 ]);
